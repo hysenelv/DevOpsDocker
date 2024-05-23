8612a671d3aa51f8eaf7206ffa96a51eec07e6e8
Elvis Hyseni <hysenelv@students.zhaw.ch>
2024-05-24 01:09:13 +0200

    change 3

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-res.send('Hello Elvis, du hast es geschafft, sogar die Verknüpfung!!!');
+res.send('Hello Elvis, du hast es geschafft, sogar die Verknüpfung <3 <3 <3 !!!');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
